3343560732668b4e3ea844e027054067318a6aaa
soroushamdg <soroushmpour@gmail.com>
2022-10-28 18:58:19 -0400

    Score board and coins board

--- Slot Machine/ContentView.swift
+++ Slot Machine/ContentView.swift
@@ -8,14 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack{
+                LinearGradient(gradient: Gradient(colors: [Color("ColorPink"),Color("ColorPurple")]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+                VStack(alignment: .center,spacing: 5) {
+                   LogoView()
+                    
+                    Spacer()
+                    HStack{
+                        HStack{
+                            Text("Your\nCoins".uppercased())
+                                .scoreLabelStyle()
+                                .multilineTextAlignment(.trailing)
+                            
+                            Text("100")
+                                .scoreNumberStyle()
+                                .modifier(ScoreNumberModifier())
+                                
+                        }
+                        .modifier(ScoreContainerModifier())
+                        
+                        Spacer()
+                        
+                        HStack{
+                            
+                            Text("200")
+                                .scoreNumberStyle()
+                                .modifier(ScoreNumberModifier())
+                            Text("High\nScore".uppercased())
+                                .scoreLabelStyle()
+                                .multilineTextAlignment(.trailing)
+                        }
+                        .modifier(ScoreContainerModifier())
+
+                    }
+                    Spacer()
+                }
+                .overlay(
+                    Button(action: {
+                        print("Reset the game")
+                    }, label: {
+                        Image(systemName: "arrow.2.circlepath.circle")
+                    }
+                    
+                )
+                    .modifier(ButtonModifier())
+                    ,alignment: .topLeading
+                )
+                .overlay(
+                    Button(action: {
+                        print("Info View")
+                    }, label: {
+                        Image(systemName: "info.circle")
+                    }
+                    
+                )
+                    .modifier(ButtonModifier())
+                    ,alignment: .topTrailing
+                )
+                .padding()
+                .frame(maxWidth: 720)
+            }
         }
-        .padding()
     }
 }
 
@@ -24,3 +81,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ Slot Machine/Helpers/Modifiers.swift
@@ -0,0 +1,45 @@
+//
+//  Modifiers.swift
+//  Slot Machine
+//
+//  Created by Soro on 2022-10-28.
+//
+
+import SwiftUI
+
+struct ShadowModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: Color("ColorTransparentBlack"),radius: 0,x: 0,y: 6)
+    }
+}
+
+struct ButtonModifier: ViewModifier{
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .accentColor(.white)
+    }
+}
+
+struct ScoreNumberModifier: ViewModifier{
+    func body(content: Content) -> some View {
+        content
+        .shadow(color:Color("ColorTransparentBlack"), radius: 0,x: 0,y: 3)
+        .layoutPriority(1)
+    }
+}
+
+
+struct ScoreContainerModifier: ViewModifier{
+    func body(content: Content) -> some View {
+        content
+            .padding(.vertical,4)
+            .padding(.horizontal,16)
+            .frame(minWidth: 128)
+            .background(
+            Capsule()
+                .foregroundColor(Color("ColorTransparentBlack"))
+            )
+    }
+}
