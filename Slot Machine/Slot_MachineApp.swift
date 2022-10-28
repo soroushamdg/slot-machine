82657b037ccc7b89a240d887da455ae3f579e107
soroushamdg <soroushmpour@gmail.com>
2022-10-28 18:16:49 -0400

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Soro on 2022-10-28.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
