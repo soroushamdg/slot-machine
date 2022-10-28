1de7fbe9b86c90bb82f4a7bb5afc63e0d9399762
soroushamdg <soroushmpour@gmail.com>
2022-10-28 19:41:22 -0400

    feat: chip in buttons created

--- Slot Machine/ContentView.swift
+++ Slot Machine/ContentView.swift
@@ -91,6 +91,48 @@ struct ContentView: View {
                     .layoutPriority(2)
                     
                     Spacer()
+                    
+                    HStack{
+                        HStack(alignment: .center,spacing: 10)  {
+                            Button {
+                                print("Bet 20 coins")
+                            } label: {
+                                Text("20")
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.white)
+                                    .modifier(BetNumberModifier())
+                            }
+                            .modifier(BetCapsuleModifier())
+                        
+                            
+                            Image("gfx-casino-chips")
+                                .resizable()
+                                .opacity(0)
+                                .modifier(CasinoChipsModifier())
+                        }
+                        
+                        HStack(alignment: .center,spacing: 10)  {
+                            
+                        
+                            
+                            Image("gfx-casino-chips")
+                                .resizable()
+                                .opacity(1)
+                                .modifier(CasinoChipsModifier())
+                            
+                            Button {
+                                print("Bet 10 coins")
+                            } label: {
+                                Text("10")
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.yellow)
+                                    .modifier(BetNumberModifier())
+                            }
+                            .modifier(BetCapsuleModifier())
+                        }
+
+                    }
+                    
                 }
                 .overlay(
                     Button(action: {
