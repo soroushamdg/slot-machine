7d7cde193c76c453050ba667f5291f0e496c9ce9
soroushamdg <soroushmpour@gmail.com>
2022-10-29 16:41:34 -0400

    fix: fix audio player bug

--- Slot Machine/Helpers/PlaySound.swift
+++ Slot Machine/Helpers/PlaySound.swift
@@ -11,11 +11,12 @@ import AVFoundation
 var audioPlayer: AVAudioPlayer?
 
 func playSound(sound: String, type: String) {
-    if let path = Bundle.main.path(forResource: sound, ofType: type)
+    if let path = Bundle.main.path(forResource: sound, ofType: type){
         do{
-        audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
-        audioPlayer?.play()
-    }catch {
-        print("error : could not find and play sound file.")
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        }catch {
+            print("error : could not find and play sound file.")
+        }
     }
 }
